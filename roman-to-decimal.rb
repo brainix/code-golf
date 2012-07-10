7b55550e447665526b39989749e7f7c55c61d6f2
Raj Shah <brainix@gmail.com>
2012-07-10 02:02:35 -0700

    saved a few characters at the expense of maintainability  ;-)

--- roman-to-decimal.rb
+++ roman-to-decimal.rb
@@ -1,8 +1,5 @@
 h,r,d={'I'=>1,'V'=>5,'X'=>10,'L'=>50,'C'=>100,'D'=>500,'M'=>1000},gets,0
-(0..r.length).each{|n|
-x=h[r[n]]||0
+(0..r.length).each{|n|x=h[r[n]]||0
 y=h[r[n+1]]||0
-m=x>=y ?'+':'-'
-d=d.send(m.to_sym,x)
-}
+d=d.send((x>=y ?'+':'-').to_sym,x)}
 puts d
