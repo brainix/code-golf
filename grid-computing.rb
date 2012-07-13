5645f268d3d69621dace8322c594044bccb0bad9
Raj Shah <brainix@gmail.com>
2012-07-12 17:06:08 -0700

    shaved off a few characters

--- grid-computing.rb
+++ grid-computing.rb
@@ -1,4 +1,4 @@
 a,t=Array,10;g,o=a.new(t){a.new(t)},0
 t.times{|x|r=gets.split.map{|e|e.to_i};t.times{|y|g[x][y]=r[y]}}
 t.times{|x|p,q=0,0;t.times{|y|p+=g[x][y];q+=g[y][x]};o=o>p ?o:p;o=o>q ?o:q}
-puts o
+p o
